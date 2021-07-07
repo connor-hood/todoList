f1b1ee19d5d1a07a3975775136d4c9854daa0c3b
connor-hood <connor_hood@yahoo.com>
2021-07-07 09:30:08 -0500

    Starting code along over

---
+++ src/components/InputTodo.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class InputTodo extends Component {
+}
+export default InputTodo
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
+
+class TodoContainer extends React.Component {
+   
+    render() {
+        return (
+            <div>
+                <h1>Hello from React</h1>
+            </div>
+        );
+    }
+}
+export default TodoContainer
